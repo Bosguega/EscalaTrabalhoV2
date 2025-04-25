a6b7f633ca2c328822e0231de5df3662c81193fc
Bosguega <bosguega@gmail.com>
2025-04-25 20:27:53 -0300

    correcoes pwa

--- vite.config.ts
+++ vite.config.ts
@@ -4,27 +4,52 @@ import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/EscalaTrabalhoV2/',
+  base: './',
   plugins: [
     vue(),
     VitePWA({
       registerType: 'autoUpdate',
-      includeAssets: ['favicon.ico', 'robots.txt', 'icons/*.svg'],
+      includeAssets: ['favicon.ico', 'robots.txt', 'icons/*.svg', 'icons/*.png'],
+      strategies: 'generateSW',
+      workbox: {
+        globPatterns: ['**/*.{js,css,html,ico,png,svg}'],
+        runtimeCaching: [
+          {
+            urlPattern: /^https:\/\/fonts\.googleapis\.com\/.*/i,
+            handler: 'CacheFirst',
+            options: {
+              cacheName: 'google-fonts-cache',
+              expiration: {
+                maxEntries: 10,
+                maxAgeSeconds: 60 * 60 * 24 * 365 // <== 365 days
+              },
+              cacheableResponse: {
+                statuses: [0, 200]
+              }
+            }
+          }
+        ]
+      },
       manifest: {
         name: 'Escala de Trabalho',
         short_name: 'Escala',
         description: 'Aplicativo de gerenciamento de escala de trabalho',
         theme_color: '#3b82f6',
+        start_url: './index.html',
+        display: 'standalone',
+        background_color: '#ffffff',
         icons: [
           {
-            src: '/icons/icon-192x192.png',
+            src: './icons/icon-192x192.png',
             sizes: '192x192',
-            type: 'image/png'
+            type: 'image/png',
+            purpose: 'any maskable'
           },
           {
-            src: '/icons/icon-512x512.png',
+            src: './icons/icon-512x512.png',
             sizes: '512x512',
-            type: 'image/png'
+            type: 'image/png',
+            purpose: 'any maskable'
           }
         ]
       }
